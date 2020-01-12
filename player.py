e47f845dee70fc027297f2d6236032e02f1f7560
Bartłomiej Górkiewicz <bartlomiej.gorkiewicz@gmail.com>
2020-01-12 17:23:54 +0100

    Adding player geters and seters

--- player.py
+++ player.py
@@ -1,11 +1,12 @@
 from db_connection import Db_connection as db
 
 class Player:
-    def __init__(self, name, socket):
+    def __init__(self, name):
         self.name = name
-        self.socket = socket
+        self.status = "Online"
         self.mmr = int(1000)
         self.id = 0
+        
         self.load_info()
 
     def __del__(self):
@@ -23,8 +24,17 @@ class Player:
     def set_mmr(self, mmr):
         self.mmr = int(mmr)
 
-    def get_socket(self):
-        return self.socket
-
+    def get_mmr(self):
+        return self.mmr
+        
     def get_id(self):
         return self.id
+
+    def set_status(self, status):
+        self.status = status
+
+    def get_status(self):
+        return self.status
+
+    def get_name(self):
+        return self.name
